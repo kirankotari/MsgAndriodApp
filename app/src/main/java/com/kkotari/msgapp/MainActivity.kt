39e65efe4f54a441d57d1e7b1f3429573b093e99
kkotari <kotarikirankumar@gmail.com>
2018-07-26 21:24:24 +0530

    adding navigation to second activity

--- app/src/main/java/com/kkotari/msgapp/MainActivity.kt
+++ app/src/main/java/com/kkotari/msgapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kkotari.msgapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +15,15 @@ class MainActivity: AppCompatActivity() {
         btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked..!")
             Toast.makeText(this, "Button was clicked..!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Log.i("MainActivity", "Second button was clicked..! with msg: $message")
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
