edcf4da5d6ba55b99a92a9799c20c27e1a0a336d
kkotari <kotarikirankumar@gmail.com>
2018-07-26 21:00:19 +0530

    adding button log and toast message

--- app/src/main/java/com/kkotari/msgapp/MainActivity.kt
+++ app/src/main/java/com/kkotari/msgapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kkotari.msgapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked..!")
+            Toast.makeText(this, "Button was clicked..!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
